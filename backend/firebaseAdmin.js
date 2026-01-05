c75f6f51fcab1fcd4a10d93818be09685aa228c6
indxr604 <inderjeet221777@gmail.com>
2026-01-05 21:45:00 +0530

    FirebaseAdmin file updated

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -3,7 +3,11 @@ const serviceAccount = require("./serviceAccountKey.json");
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
