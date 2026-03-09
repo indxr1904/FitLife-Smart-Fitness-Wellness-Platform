a51bb937e2be663c58b316dfe11cd510c0bf10e7
indxr604 <inderjeet221777@gmail.com>
2026-03-09 19:41:24 +0530

    refactor admin panel

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   "https://fitlife-smart-fitness-wellness-platform.onrender.com";
 
-// export const API_BASE_URL = "localhost:3000";
+// export const API_BASE_URL = "http://localhost:3000";
